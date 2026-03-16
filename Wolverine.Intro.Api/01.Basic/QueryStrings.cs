aed393d82cc71537ce9d187835f50af241e4313e
Lodewijk Sioen <lodewijk.sioen@codit.eu>
2026-03-16 15:16:10 +0100

    Test QueryString handling

---
+++ Wolverine.Intro.Api/01.Basic/QueryStrings.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wolverine.Http;
+
+// Wolverine does't like numbers in the namespace
+// ReSharper disable once CheckNamespace
+namespace Wolverine.Intro.Api.Basic;
+
+public class QueryStrings
+{
+    [WolverineGet("/querystring/simple/raw")]
+    public string Simple(string code)
+    {
+        return code;
+    }
+
+    [WolverineGet("/querystring/simple/attr")]
+    public string SimpleAttr([FromQuery] string code)
+    {
+        return code;
+    }
+
+    [WolverineGet("/querystring/simple/attrNamed")]
+    public string SimpleNamed([FromQuery(Name = "c")] string code)
+    {
+        return code;
+    }
+
+    [WolverineGet("/querystring/collection/raw")]
+    public string[] Collection(string[] codes)
+    {
+        return codes;
+    }
+
+    /*
+     DOES NOT COMPILE
+     Is not the Wolverine way of doing stuff
+    [WolverineGet("/querystring/collection/attr")]
+    public string[] CollectionAttr([FromQuery]string[] codes)
+    {
+        return codes;
+    }
+
+    [WolverineGet("/querystring/collection/attrNamed")]
+    public string[] CollectionAttrNamed([FromQuery(Name = "c")] string[] codes)
+    {
+        return codes;
+    }
+    */
+
+    public record ComplexType([FromQuery]string Code, [FromQuery] string[] Codes);
+    [WolverineGet("/querystring/complex/attr")]
+    public object ComplexAttr([FromQuery]ComplexType complex)
+    {
+        return new { complex.Code, complex.Codes };
+    }
+
+    public record ComplexTypeWithAttribute([FromQuery(Name = "c")] string Code, [FromQuery(Name = "cs")]string[] Codes);
+    [WolverineGet("/querystring/complex/attrNamed")]
+    public object ComplexNamed([FromQuery]ComplexTypeWithAttribute complex)
+    {
+        return new { complex.Code, complex.Codes };
+    }
+}
---
+++ Wolverine.Intro.Tests/01.Basic/QueryStringTests.cs
@@ -0,0 +1,72 @@
+﻿using Alba;
+
+namespace Wolverine.Intro.Tests._01.Basic;
+
+public class QueryStringTests
+{
+    [Test]
+    public async Task SimpleRaw()
+    {
+        await SystemUnderTest.Host.Scenario(s =>
+        {
+            s.Get.Url("/querystring/simple/raw?code=test");
+            s.StatusCodeShouldBe(200);
+            s.ContentShouldBe("test");
+        });
+    }
+
+    [Test]
+    public async Task SimpleAttribute()
+    {
+        await SystemUnderTest.Host.Scenario(s =>
+        {
+            s.Get.Url("/querystring/simple/attr?code=test");
+            s.StatusCodeShouldBe(200);
+            s.ContentShouldBe("test");
+        });
+    }
+
+    [Test]
+    public async Task SimpleAttributeNamed()
+    {
+        await SystemUnderTest.Host.Scenario(s =>
+        {
+            s.Get.Url("/querystring/simple/attrNamed?c=test");
+            s.StatusCodeShouldBe(200);
+            s.ContentShouldBe("test");
+        });
+    }
+
+    [Test]
+    public async Task CollectionRaw()
+    {
+        await SystemUnderTest.Host.Scenario(s =>
+        {
+            s.Get.Url("/querystring/collection/raw?codes=test1&codes=test2&codes=test3");
+            s.StatusCodeShouldBe(200);
+            s.ContentShouldBe("""["test1","test2","test3"]""");
+        });
+    }
+
+    [Test]
+    public async Task ComplexAttribute()
+    {
+        await SystemUnderTest.Host.Scenario(s =>
+        {
+            s.Get.Url("/querystring/complex/attr?code=test&codes=test1&codes=test2&codes=test3");
+            s.StatusCodeShouldBe(200);
+            s.ContentShouldBe("{\"code\":\"test\",\"codes\":[\"test1\",\"test2\",\"test3\"]}");
+        });
+    }
+
+    [Test]
+    public async Task ComplexAttributeNamed()
+    {
+        await SystemUnderTest.Host.Scenario(s =>
+        {
+            s.Get.Url("/querystring/complex/attrNamed?c=test&cs=test1&cs=test2&cs=test3");
+            s.StatusCodeShouldBe(200);
+            s.ContentShouldBe("{\"code\":\"test\",\"codes\":[\"test1\",\"test2\",\"test3\"]}");
+        });
+    }
+}
